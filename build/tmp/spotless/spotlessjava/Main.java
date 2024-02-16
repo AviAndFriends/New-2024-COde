501b01db30533e2b5dc749080b0da6cb9820bc0c
AviAndFriends <team7460@sylvaniastem.org>
2024-02-15 19:11:56 -0500

    Initial commit

---
+++ build/tmp/spotless/spotlessjava/Main.java
@@ -0,0 +1,13 @@
+package build.tmp.spotless.spotlessjava;
+
+public class main {
+    public static void main(String... args) {
+        RobotBase.startRobot(Robot::new);
+      }
+    }
+    
+}
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.RobotBase;
